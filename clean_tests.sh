2a2e38a9b9771a9da5c98b3a5479449b12865111
gabcat <gabriele.catasta@mail.polimi.it>
2026-09-15 17:14:41 +0200

    Add script to clean IR modules preserved for inspection

---
+++ clean_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ 
+rm -f *.ll *.ptx *.fatbin
+rm -f compiled_eddi_functions.csv compiled_cfcss_functions.csv
+rm -f saxpy_out axpy_out conv_out conv_ref
+rm -f axpy_b.txt axpy_k.txt axpy_t.txt out.dat
